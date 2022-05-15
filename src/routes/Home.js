a196e186b4ab6a468083b5001810ab396cd0fdc1
Viktor Farkas <fv1ktor09@gmail.com>
2022-05-15 16:32:55 +0200

    added mix

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -73,6 +73,20 @@ const Home = () => (
       </div>
     </section>
 
+    <section className="section mixes">
+      <h2 className="item-title">mixes from home</h2>
+      <div className="embedContainer">
+        <iframe
+          title="mixes from home #01 - power up on Soundcloud"
+          className="embedSong"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1268380525&color=%231a1a1a&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+        </iframe>
+      </div>
+    </section>
+
     <section className="section inspiration">
       <h2 className="item-title">where I get inspired from</h2>
       <div className="embedContainer">
